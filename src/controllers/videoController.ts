8444a5ea878d21f26eebd70635685218c4fc351d
Genious-Bajracharya <102010683+Genious-Bajracharya@users.noreply.github.com>
2025-08-10 12:37:07 +0545

    updated vid controller

--- src/controllers/videoController.ts
+++ src/controllers/videoController.ts
@@ -1,9 +1,25 @@
 import { Request, Response } from "express";
 import Videos from "../models/Videos";
+import Course from "../models/Course";
 
+// Create 
 export const createVideo = async (req: Request, res: Response) => {
-  const video = await Videos.create(req.body);
-  res.status(201).json(video);
+  const { title, url, courseId } = req.body;
+  try {
+    const video = await Videos.create({ title, url });
+    await Course.findByIdAndUpdate(courseId, { $push: { videos: video._id } });
+    res.status(201).json(video);
+  } catch (err) {
+    res.status(500).json({ message: "Error creating video" });
+  }
+};
+
+// Delete
+export const deleteVideo = async (req: Request, res: Response) => {
+  const { id, courseId } = req.params;
+  await Videos.findByIdAndDelete(id);
+  await Course.findByIdAndUpdate(courseId, { $pull: { videos: id } });
+  res.json({ message: "Video deleted" });
 };
 
 export const getVideos = async (req: Request, res: Response) => {
